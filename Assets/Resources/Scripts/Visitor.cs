5da3ac31091d2e0465a0d87f32ab3e6cae68fd3c
Sarah Haefele <sarah.haefele@hs-furtwangen.de>
2015-06-14 20:10:09 +0200

    Added highlighting script

---
+++ Assets/Resources/Scripts/Visitor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*The Visitor checks for exhibits*/
+
+public class Visitor : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void checkForExhibits()
+    {
+
+    }
+}
